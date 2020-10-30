f997522d53c7f301ccffad40323fd38847223021
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-10-30 12:27:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>รมิตา แพสถิตถาวร</h1>
+        <p>แพรวต๋อย</p>
+        <img src="cat.jpg">
+        <img src="shotaro.jpg">
+        <br>
+        <a href= "http://nct.smtown.com/"> This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazom</li>
+        </ul>
+
+    </body>
+</html>
