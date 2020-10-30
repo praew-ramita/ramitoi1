da1c7827262171d32c07a12cd6de97617e07491e
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-10-30 12:28:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,6 @@
     <body>
         <h1>รมิตา แพสถิตถาวร</h1>
         <p>แพรวต๋อย</p>
-        <img src="cat.jpg">
-        <img src="shotaro.jpg">
-        <br>
         <a href= "http://nct.smtown.com/"> This is a Link</a>
-
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazom</li>
-        </ul>
-
     </body>
 </html>
