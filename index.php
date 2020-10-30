b087612de586fb0dbf291e4f8dfa219b47165459
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-10-30 12:31:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <body>
         <h1>รมิตา แพสถิตถาวร</h1>
         <p>แพรวต๋อย</p>
+        <img src="cat.jpg">
+        <img src="shotaro.jpg">
         <a href= "http://nct.smtown.com/"> This is a Link</a>
     </body>
 </html>
