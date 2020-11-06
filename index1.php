3d0f55ca2826c3575a490e068472f744ef21881d
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-11-06 16:47:29 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LAB-ITF</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
-                <img src="cat5.jpg" style="height:300px;" style="width: 100px;"class="img-fluid rounded" />
+                <img src="cat1.jpg" style="height:300px;" style="width: 100px;" class="img-fluid rounded" />
             </div>
             <div class="col-sm-3">
-                <img src="cat.jpg" style="height:100px;" style="width: 50px;"class="img-fluid img-thumbnail" />
-                <img src="cat1.jpg" style="height:100px;" style="width: 50px;"class="img-fluid img-thumbnail" />
-                <img src="cat2.png" style="height:100px;" style="width: 50px;"class="img-fluid img-thumbnail" />
+                <img src="cat.jpg" style="height:100px;" style="width: 50px;" class="img-fluid img-thumbnail" />
+                <img src="cat3.jpg" style="height:100px;" style="width: 50px;" class="img-fluid img-thumbnail" />
+                <img src="cat4.jpg" style="height:100px;" style="width: 50px;" class="img-fluid img-thumbnail" />
             </div>
             <div class="col-sm-3">
-                <img src="cat3.jpg" style="height: 150px;" style="width: 50px;" class="img-fluid" />
-                <img src="cat4.jpg" style="height: 150px;" style="width: 50px;" class="img-fluid img-thumbnail" />
+                <img src="cat5.jpg" style="height: 200px;" style="width: 50px;" class="img-fluid" />
+                <img src="cat6.jpg" style="height: 100px;" style="width: 50px;" class="img-fluid img-thumbnail" />
             </div>
         </div> <!-- Close Row -->
     </div> <!-- Close container -->
 </body>
+
 </html>
