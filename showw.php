b8f9278ea0ed3b2783df1589f390e1a0ecf06799
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-11-18 23:27:04 +0700

    Update showw.php

--- showw.php
+++ showw.php
@@ -44,7 +44,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   }
   ?>
   </table>
-  <a href="forminsert.html"><input type="submit" value="ADD" class="btn-default btn-sm"></a>
+  <a href="forminsert.php"><input type="submit" value="ADD" class="btn-default btn-sm"></a>
   <?php
   mysqli_close($conn);
   ?>
