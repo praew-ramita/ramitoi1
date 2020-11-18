8bd62f9dd8f25bc1cf874fab4553ee73ed742073
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-11-18 23:24:25 +0700

    Update insert1.php

--- insert1.php
+++ insert1.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'lab02.mysql.database.azure.com', 'woranun@lab02', 'VGTlfl45', 'itflab', 3306);
+mysqli_real_connect($conn, 'ramitoi.mysql.database.azure.com', 'praewramita@ramitoi', 'Rt0877003141', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
