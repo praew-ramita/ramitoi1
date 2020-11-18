7a82f78cb6c2723512b2b1a14a32bd86e2f18c59
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-11-18 23:16:50 +0700

    Update showw.php

--- showw.php
+++ showw.php
@@ -11,7 +11,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'tonklazz.mysql.database.azure.com', 'tonklazuzu2@tonklazz', 'Kla007as', 'itflab', 3306);
+mysqli_real_connect($conn, 'ramitoi.mysql.database.azure.com', 'praewramita@ramitoi', 'Rt0877003141', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL PLEASE TRY AGAIN !: '.mysqli_connect_error());
