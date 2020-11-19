9105cd0301a87c27c473bf94204247972234f016
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-11-19 15:12:42 +0700

    Update formedit.php

--- formedit.php
+++ formedit.php
@@ -6,9 +6,9 @@
 </head>
 <body>
   <form action = "edit2.php" method = "post" id="CommentForm" >
-    ชื่อของคนที่จะแก้:<br>
+    What's name do you want to change?:<br>
     <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
-    ความรู้สึกที่เปลี่ยนไป:<br>
+    Your new emotion!:<br>
     <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter your feel"></textarea><br>  
     <input type="submit" id="commentBtn">
   </form> 
