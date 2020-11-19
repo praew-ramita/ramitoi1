dba18ecd172d5ea1fe1ef413ca79edc4c6e1c68a
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-11-19 14:58:26 +0700

    Update showw.php

--- showw.php
+++ showw.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>How are you today</title>
+<title>What's your day going?</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -21,13 +21,13 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 
 <div class="container">
     <div class="form">
-        <p class="ex1"><h1><center>How are you today</center></h1></p>
+        <p class="ex1"><h1><center>What's your day going?</center></h1></p>
     </div>
   <table width="800" border="1" class="table table-white table-hover table-striped" class="center" >
     <thead class="thead-dark">
     <tr class="active">
       <th width="200"> <div align="center">Name</div></th>
-      <th width="500"> <div align="center">Comment:)</div></th>
+      <th width="500"> <div align="center">Comment :)</div></th>
       <th width="100"> <div align="center">Action</div></th>
     </tr>
      </thead>
